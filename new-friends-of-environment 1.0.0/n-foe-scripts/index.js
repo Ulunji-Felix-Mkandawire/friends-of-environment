76734acb13f84e333fc5caa248bdcf64ebbfb438
Ulunji-Felix-Mkandawire <145837309+Ulunji-Felix-Mkandawire@users.noreply.github.com>
2023-09-23 11:39:37 +0200

    Delete new-friends-of-environment 1.0.0 directory

--- new-friends-of-environment 1.0.0/n-foe-scripts/index.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector(".toggle-btn");
-
-toggleBtn.addEventListener("click", ()=>{
-    const navLinks = document.querySelector(".nav-links");
-    toggleBtn.classList.toggle("active")
-    navLinks.classList.toggle("active");
-})
